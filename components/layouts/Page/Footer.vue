e5e218ef21fddc87f5e73cb93f31a7007321c35a
ranlin <mumu@irlin.cn>
2024-07-02 21:43:58 +0800

    fix footer

--- components/layouts/Page/Footer.vue
+++ components/layouts/Page/Footer.vue
@@ -1,29 +1,24 @@
 <script lang="ts" setup>
+import gongan from '~/assets/images/about/gongan.png'
 const { awesome } = useAppConfig()
 </script>
 
 <template>
-  <footer class=" border-t bg-[#3F4F71] pt-[50px] pb-[25px]  dark:border-gray-50/[0.2]">
+  <footer class=" border-t bg-[#3F4F71]   dark:border-gray-50/[0.2]">
     <div
-      class="flex gap-[15px] flex-col items-center justify-center   text-[14px]  font-normal text-center text-white leading-[16px]">
-      <div>
-        <span>
-          版权所有：北京行展软件科技有限公司
-        </span>
-        <span>
-          京ICP备18007287号
-        </span>
-        <span>京公网安备10000000000001号</span>
-      </div>
-      <div>
-        <span> 京公网安备 50010302000681号</span>
+      class="flex flex-row h-[107px] gap-[15px]  items-center justify-center   text-[14px]  font-normal text-center text-white leading-[16px]">
 
-      </div>
-      <div>
-        <span>
-          北京行展软件有限公司
-        </span>
-      </div>
+      <span>
+        版权所有：北京行展软件科技有限公司
+      </span>
+      <span>
+        京ICP备18007287号
+      </span>
+      <span class="flex items-center gap-[3px]"> <img :src="gongan" /> 京公网安备10000000000001号
+      </span>
+
+
+
     </div>
   </footer>
 </template>
